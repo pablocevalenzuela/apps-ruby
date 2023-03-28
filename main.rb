6794a99f5279b0baad5510529c81acf43088af9f
Pablo Valenzuela <pabloce.valenzuela@gmail.com>
2023-03-27 23:44:01 -0300

    Mejoras en pieza main

--- main.rb
+++ main.rb
@@ -1,21 +1,33 @@
 class Word
     @word_input = ''
+    @ac = ''
     
     def initialize(defaultMessage = '¡Enter a word for to be auto-completed for you!')
         @defaultMessage = defaultMessage
     end     
 
-    def input(pw)
+    def input(pw = '')
         @word_input = pw
-        if @word_input.nil?
-        puts @defaultMessage
+        if @word_input.empty?
+            puts @defaultMessage
         else
-        puts @word_input
+         case @ac = @word_input
+            when "h"
+             puts "Hello"
+            when "hay"   
+             puts "How are you?"
+            when "ift"
+             puts "Is for today"
+            when "cm"
+                puts "¡Call me, please!."
+            when "tfth"
+                puts "¡Thank you for to be here!."
+            else
+             puts "Unregistered acronyms."
+         end    
         end     
     end 
-
 end     
+word = Word.new
 
-s = Word.new
-
-s.input(nil)
+word.input('tfth')
