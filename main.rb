9bfe56a0db55a09ce61dd0f4fc996cfbe0b1963e
Pablo Valenzuela <pabloce.valenzuela@gmail.com>
2023-04-10 21:41:36 -0400

    Updated Main

--- main.rb
+++ main.rb
@@ -1,34 +1,41 @@
-class Word
+class Shortcut
     @word_input = ''
     @ac = ''
+    @phone_number = 0
     
-    def initialize(defaultMessage = '¡Enter a word for to be auto-completed for you!')
-        @defaultMessage = defaultMessage
-    end     
+    def initialize(default_message = '¡Enter a word for to be auto-completed for you!')
+        @default_message = default_message
+    end
+    
+    def set_my_phone_number(my_number = '912345678')
+        @phone_number = my_number
+    end
 
-    def input(pw = '')
+
+    def set_words(pw = '')
         @word_input = pw
         if @word_input.empty?
-            puts @defaultMessage
+            puts @default_message
         else
         case @ac = @word_input
-        when "h"
+            when "h"
             puts "Hello"
             when "hay"   
             puts "How are you?"
             when "ift"
             puts "Is for today"
             when "cm"
-            puts "¡Call me, please!."
+            puts "¡Please, call me to +56 9 #{@phone_number}!."
             when "tfth"
             puts "¡Thank you for to be here!."
             else
             puts "Unregistered acronyms."
-         end    
+            end    
         end     
     end 
 end     
 
-word = Word.new
+shortcut = Shortcut.new
+shortcut.set_my_phone_number("87898888")
 
-word.input('tfth')
+shortcut.set_words('cm')
